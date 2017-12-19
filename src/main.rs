158dbb995c1f562f2a8f5326ca99caa0741e2ad1
Chen Weiguang <chen.weiguang@gmail.com>
2017-12-19 14:06:24 +0800

    Updated dep and using custom fruently.

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@
 #![cfg_attr(feature = "clippy", deny(warnings))]
 #![feature(slice_patterns)]
 
-#[macro_use]
 extern crate failure;
 extern crate fruently;
 extern crate serde;
@@ -17,17 +16,6 @@ use fruently::fluent::Fluent;
 use fruently::forwardable::JsonForwardable;
 use serde_json::Value;
 use structopt::StructOpt;
-
-#[derive(Debug, Fail)]
-enum FluentError {
-    #[fail(display = "")] InnerFluentError { e: fruently::error::FluentError },
-}
-
-impl From<fruently::error::FluentError> for FluentError {
-    fn from(e: fruently::error::FluentError) -> FluentError {
-        FluentError::InnerFluentError { e: e }
-    }
-}
 
 type Result<T> = std::result::Result<T, Error>;
 
@@ -53,7 +41,7 @@ fn run() -> Result<()> {
     let v: Value = serde_json::from_str(&config.value)?;
     let fruently = Fluent::new(config.addr.as_str(), config.tag.as_str());
 
-    fruently.post(&v).map_err(|e| -> FluentError { e.into() })?;
+    fruently.post(&v)?;
 
     Ok(())
 }
